896e9341ae8a54c4c9e998c44a935e602f092517
Joydip <joydip.mondal@gmail.com>
2024-12-19 16:38:57 +0530

    Update product.service.ts

--- codes/day-4/pms-app/src/app/modules/products/services/product.service.ts
+++ codes/day-4/pms-app/src/app/modules/products/services/product.service.ts
@@ -10,8 +10,20 @@ export class ProductService implements ServiceContract {
     constructor(private http: HttpClient) {
 
     }
+    getProductById(id: number): Observable<ApiResponse<Product>> {
+        return this.http.get<ApiResponse<Product>>(`${API_URL}/${id}`)
+    }
+    addProduct(p: Product): Observable<ApiResponse<Product[]>> {
+        return this.http.post<ApiResponse<Product[]>>(API_URL, p)
+    }
+    updateProduct(id: number, p: Product): Observable<ApiResponse<Product[]>> {
+        return this.http.put<ApiResponse<Product[]>>(`${API_URL}/${id}`, p)
+    }
+    deleteProduct(id: number): Observable<ApiResponse<Product[]>> {
+        return this.http.delete<ApiResponse<Product[]>>(`${API_URL}/${id}`)
+    }
 
-    getProducts() {
+    getProducts(): Observable<ApiResponse<Product[]>> {
         // const apiResponse: Observable<ApiResponse<Product[]>> = this.http.get<ApiResponse<Product[]>>(API_URL)
         // return apiResponse
         return this.http.get<ApiResponse<Product[]>>(API_URL)
