a232cd982510f838e80dab3378a4bebbb5813b68
Joydip <joydip.mondal@gmail.com>
2024-12-19 16:32:28 +0530

    service contract updated

--- codes/day-4/pms-app/src/app/modules/products/services/product.service.ts
+++ codes/day-4/pms-app/src/app/modules/products/services/product.service.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from "@angular/common/http";
 import { Product } from "../../../models/product";
 import { ServiceContract } from "./servicecontract";
+import { API_URL } from "../../../utils/constants";
+import { ApiResponse } from "../../../models/apiresponse";
+import { Observable } from "rxjs";
 
 export class ProductService implements ServiceContract {
+
+    constructor(private http: HttpClient) {
+
+    }
+
     getProducts() {
-        return []
+        // const apiResponse: Observable<ApiResponse<Product[]>> = this.http.get<ApiResponse<Product[]>>(API_URL)
+        // return apiResponse
+        return this.http.get<ApiResponse<Product[]>>(API_URL)
     }
 }
