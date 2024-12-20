9014fcdf46f70bf8156efa1fedd69dbecc228dc2
Joydip <joydip.mondal@gmail.com>
2024-12-20 14:34:19 +0530

    view product design update

--- codes/day-5/pms-app/src/app/modules/products/components/view-product/view-product.component.ts
+++ codes/day-5/pms-app/src/app/modules/products/components/view-product/view-product.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { SERVICE_TOKEN } from '../../../../utils/constants';
 import { ServiceContract } from '../../services/servicecontract';
 import { Subscription } from 'rxjs';
@@ -10,7 +10,7 @@ import { ApiResponse } from '../../../../models/apiresponse';
   templateUrl: './view-product.component.html',
   styleUrl: './view-product.component.css'
 })
-export class ViewProductComponent implements OnInit {
+export class ViewProductComponent implements OnInit, OnDestroy {
 
   private sub?: Subscription;
 
@@ -20,10 +20,19 @@ export class ViewProductComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    @Inject(SERVICE_TOKEN) private ps: ServiceContract
+    @Inject(SERVICE_TOKEN) private ps: ServiceContract,
+    private router: Router
   ) {
 
   }
+  update(id: number) {
+    this.router.navigate(['/products', 'update', id])
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe()
+  }
+
   ngOnInit(): void {
     const snapshot: ActivatedRouteSnapshot = this.route.snapshot
     //const id = +snapshot.params['id']
